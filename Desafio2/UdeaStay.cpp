85cb9d4078500f72cd6ee248fdbdd14555dd1d81
Alejandro <alejandro.alvarezp@udea.edu.co>
2025-05-25 19:29:48 -0500

    Implementacion iteracciones externas e internas y memoria usada en la clase login

--- Desafio2/UdeaStay.cpp
+++ Desafio2/UdeaStay.cpp
@@ -2,38 +2,53 @@
 
 //metodos Clase Login
 string Login::getterWel(){
-    bienvenidad="||||Bienvenido a UdeAStay||||\n";
+    bienvenidad="||||Bienvenido a UdeAStay||||\n\n";
     return bienvenidad;
 }
 
 bool Login::verificate(string user,string pass, string nombrearchivo){
+    int con=0;
+    con++;
     int n=10;
+    con++;
     string copia[n];
+    con++;
     char c;
+    con++;
     bool valida;
+    con++;
     int cont=0;
+    con++;
     ifstream archivo(nombrearchivo);
+    con++;
     if(!archivo.is_open()){
+        con++;
         cerr<<"Error: no se abrio el archivo\n";
+        con++;
     }
-    else
-        cout<<"Archivo Abierto correctamente"<<endl;
     for (int i = 0; i < n; ++i) {
         while (archivo.get(c)) {
             if(c==','||c=='\n'){
+                con++;
                 break;
                 }
+            con++;
             copia[i]+=c;
         }
         cont++;
+        con++;
         if (copia[i]=="")
+            con++;
             break;
     }
 
     copia[cont-1]='\0';
+    con++;
     for (int e = 0; e<n; e+=2) {
         if(copia[e]==user&&copia[e+1]==pass){
+            con++;
             valida=true;
+            con++;
             break;
         }
     }
@@ -41,7 +56,8 @@ bool Login::verificate(string user,string pass, string nombrearchivo){
 
 
     archivo.close();
-
+    con++;
+    cout<<"interacciones Internas de login: "<<con<<endl;
     return valida;
     }
 
--- Desafio2/main.cpp
+++ Desafio2/main.cpp
@@ -1,54 +1,96 @@
 #include "UdeaStay.h"
-#include <iostream>
+
 
 using namespace std;
 
 int main()
 {
-
+    int con=0;
+    con++;
     Login welcome;
+    con++;
     cout<<welcome.getterWel();
+    cout<<"objeto Login welcome "<<sizeof(welcome)<<" Bytes"<<endl;
+    con++;
+    cout<<"interacciones welcome: "<<con++<<endl<<endl;
     int opcion = 1;
+    con=0;
+    con++;
     while(opcion!=0){
     cout<<"1.Anfitrion"<<endl;
+        con++;
     cout<<"2.Huesped"<<endl;
+        con++;
     cout<<"0.Salir"<<endl;
+        con++;
     cin>>opcion;
+        con++;
+    cout<<"Iteraciones Menu : "<<con<<endl;
     switch (opcion) {
     case 1:{
+        con=0;
         string user;
+        con++;
         string passwd;
+        con++;
         cout<<"ingrese Usuario:"<<endl;
+        con++;
         cin>>user;
         cout<<endl;
+        con++;
         cout<<"Ingrese Contraseña:"<<endl;
+        con++;
         cin>>passwd;
+        con++;
         cout<<endl;
+        con++;
         Login acceso;
         bool confirm=acceso.verificate(user,passwd,"Archivos/loginAnfitrion.txt");
+        cout<<"Objeto Login Acceso: "<<sizeof(acceso)<<" Bytes"<<endl;
         if(confirm){
+            con++;
+            cout<<"Iteracciones login externas: "<<con++<<endl;
             cout<<"Bienvenido Anfitrion\n";
         }
-        else
+        else{
             cout<<"Credenciales incorrectas"<<endl;
+       con++;
+       cout<<"Iteracciones login externas: "<<con++<<endl;
+        }
 
         break;
-    }
+       }
     case 2:{
+           con=0;
+        con++;
         string user;
+           con++;
         string passwd;
-        cout<<"ingrese Usuario:"<<endl;
-        cin>>user;
-        cout<<"Ingrese Contraseña:"<<endl;
-        cin>>passwd;
-        cout<<endl;
-        Login acceso;
-        bool confirm=acceso.verificate(user,passwd,"Archivos/loginHuesped.txt");
-        if(confirm){
-            cout<<"Bienvenido Huesped\n";
+           con++;
+           cout<<"ingrese Usuario:"<<endl;
+           con++;
+           cin>>user;
+           con++;
+           cout<<"Ingrese Contraseña:"<<endl;
+           con++;
+           cin>>passwd;
+           con++;
+           cout<<endl;
+           con++;
+           Login acceso;
+           con++;
+           bool confirm=acceso.verificate(user,passwd,"Archivos/loginHuesped.txt");
+           cout<<"Objeto Login acceso: "<<sizeof(acceso)<<" Bytes"<<endl;
+           if(confirm){
+            con++;
+           cout<<"Bienvenido Huesped\n\n";
+        cout<<"Iteraciones Login Huesped: "<<con++<<endl<<endl;
         }
-        else
-            cout<<"credenciales incorrectas\n";
+        else{
+            cout<<"credenciales incorrectas\n\n";
+            cout<<"Iteraciones Login Huesped: "<<con<<endl<<endl;
+
+        }
         break;
 
     }
